e61207ebefcf294e7ba73be5aae7de71d64d5e7f
Kabir S. Tamari <kabirtamari@gmail.com>
2023-02-04 02:04:52 +0545

    navbar and scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import './App.scss'
 import { About, Footer, Header, Skills, Testimonials, Work } from './container'
-
+import NavBar from './components/NavBar/NavBar'
 const App = () => {
     return (
         <div className='app'>
+            <NavBar />
             <Header />
             <About />
             <Work />
