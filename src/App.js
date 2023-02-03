c3b0173b3a391bfcdd7f6a4bc46f9a52a68391f1
Kabir S. Tamari <kabirtamari@gmail.com>
2023-02-03 22:17:49 +0545

    project init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { About, Footer, Header, Skills, Testimonials, Work } from './container'
+
+const App = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <About />
+            <Work />
+            <Skills />
+            <Testimonials />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
